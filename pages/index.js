415370a7d2a0811294e53cfcc15e71a455a4fecc
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-04-15 21:18:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <a href="http://www.conexaofuturo.com.br/course/view.php?id=6" target="_blank">Teorias atômicas - atividade complementare (Curso aberto)</a>
+      <a href="http://www.conexaofuturo.com.br/course/view.php?id=6" target="_blank">Teorias atômicas - atividade complementar (Curso aberto)</a>
     </div>
   )
 }
