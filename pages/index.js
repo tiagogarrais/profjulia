44594b92e31524966cb575916e758290e2a8b668
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-04-18 22:26:09 -0300

    add jogo da memoria

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <a href="/teoriasatomicas.html" target="_blank">Teorias atômicas - atividade complementar (Curso aberto)</a>
+      <br/>
+      <a href="/jogo-da-memoria-dia-do-indio.html" target="_blank">Jogo da memória - Dia do índio</a>
+
     </div>
   )
 }
