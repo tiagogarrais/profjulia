0142ac030ddf3bbf444d9301c8b08f9eecf8324f
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-04-18 22:59:35 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <div>
       <a href="/teoriasatomicas.html" target="_blank">Teorias atômicas - atividade complementar (Curso aberto)</a>
-      <br/>
+      <br />
       <a href="/jogo-da-memoria-dia-do-indio.html" target="_blank">Jogo da memória - Dia do índio</a>
-
     </div>
   )
 }
